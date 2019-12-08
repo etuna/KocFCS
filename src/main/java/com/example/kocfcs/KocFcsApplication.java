7a9c9b061d5921442b648d2e0258a05af2b82f9e
Esat Tunahan Tuna <etuna@ku.edu.tr>
2019-12-08 14:02:08 +0300

    first commit

---
+++ src/main/java/com/example/kocfcs/KocFcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kocfcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocFcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocFcsApplication.class, args);
+	}
+
+}
